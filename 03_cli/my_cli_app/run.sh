e771800771e3776ad3923551af8c1cad0c5ccd3a
Sridhar Sarnobat <ss401533@gmail.com>
2025-03-08 23:53:32 -0500

    2025-03-08: Minor changes (untested)

---
+++ 03_cli/my_cli_app/run.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#----------------------------------------------------------------------------
+# DESCRIPTION		
+# DATE				2025
+# AUTHOR			ss401533@gmail.com                                           
+#----------------------------------------------------------------------------
+# template found at ~/.vim/sh_header.temp
+
+set -o errexit
+echo "$0" >> ~/db.git/command_history.txt | ts >> ~/db.git/command_history_timestamped.txt
+
+cat <<EOF | batcat --style=plain --paging=never --language sh --theme TwoDark
+cargo new my_cli_app && cd my_cli_app
+cargo build
+target/debug/my_cli_app -i input.txt
+EOF
+
