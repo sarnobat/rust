b5670f87587512140b89aee7b5a565089596033c
Sridhar Sarnobat <ss401533@gmail.com>
2025-03-08 23:58:40 -0500

    2025-03-08: Minor changes (untested)

--- 03_cli/my_cli_app/src/main.rs
+++ 03_cli/my_cli_app/src/main.rs
@@ -3,8 +3,8 @@ use clap::{App, Arg};
 fn main() {
     let matches = App::new("My CLI App")
         .version("1.0")
-        .author("Your Name")
-        .about("A simple CLI application")
+        .author("Sridhar Sarnobat")
+        .about("A simple CLI tool written in rust")
         .arg(
             Arg::with_name("input")
                 .short("i")
@@ -15,14 +15,6 @@ fn main() {
                 .takes_value(true),
         )
         .arg(
-            Arg::with_name("output")
-                .short("o")
-                .long("output")
-                .value_name("FILE")
-                .help("Sets the output file")
-                .takes_value(true),
-        )
-        .arg(
             Arg::with_name("verbose")
                 .short("v")
                 .long("verbose")
@@ -30,13 +22,9 @@ fn main() {
         )
         .get_matches();
 
-    // Accessing the parsed arguments
-    let input_file = matches.value_of("input").unwrap(); // unwrap() is safe here because it's required
+    let input_file = matches.value_of("input").unwrap();
     println!("Input file: {}", input_file);
 
-    if let Some(output_file) = matches.value_of("output") {
-        println!("Output file: {}", output_file);
-    }
 
     if matches.is_present("verbose") {
         println!("Verbose mode enabled");
