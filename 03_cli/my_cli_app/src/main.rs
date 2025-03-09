7bb9e59b4f7acd1b3fe0cdb573980cb42024f40e
Sridhar Sarnobat <ss401533@gmail.com>
2025-03-08 23:55:40 -0500

    2025-03-08: Minor changes (untested)

--- 03_cli/my_cli_app/src/main.rs
+++ 03_cli/my_cli_app/src/main.rs
@@ -1,3 +1,44 @@
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("My CLI App")
+        .version("1.0")
+        .author("Your Name")
+        .about("A simple CLI application")
+        .arg(
+            Arg::with_name("input")
+                .short("i")
+                .long("input")
+                .value_name("FILE")
+                .help("Sets the input file")
+                .required(true)
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("output")
+                .short("o")
+                .long("output")
+                .value_name("FILE")
+                .help("Sets the output file")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("verbose")
+                .short("v")
+                .long("verbose")
+                .help("Enables verbose mode"),
+        )
+        .get_matches();
+
+    // Accessing the parsed arguments
+    let input_file = matches.value_of("input").unwrap(); // unwrap() is safe here because it's required
+    println!("Input file: {}", input_file);
+
+    if let Some(output_file) = matches.value_of("output") {
+        println!("Output file: {}", output_file);
+    }
+
+    if matches.is_present("verbose") {
+        println!("Verbose mode enabled");
+    }
 }
