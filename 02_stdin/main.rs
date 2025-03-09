6a1d5c1cbca6d8932780bc45109c9dba0ac53b76
Sridhar Sarnobat <ss401533@gmail.com>
2025-03-08 23:47:43 -0500

    2025-03-08: Minor changes (untested)

---
+++ 02_stdin/main.rs
@@ -0,0 +1,17 @@
+use std::io::{self, BufRead};
+
+fn main() {
+    let stdin = io::stdin();
+    for line in stdin.lock().lines() {
+        match line {
+            Ok(line) => {
+                println!("Read line: {}", line);
+            }
+            Err(error) => {
+                eprintln!("Error reading line: {}", error);
+                break; // Or handle the error differently
+            }
+        }
+    }
+}
+
