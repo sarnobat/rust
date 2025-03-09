16461e01a9ac7c1b5e90929a400babbed22b6650
Sridhar Sarnobat <ss401533@gmail.com>
2025-03-08 23:43:00 -0500

    2025-03-08: Minor changes (untested)

---
+++ 01_rust/run.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#----------------------------------------------------------------------------
+# DESCRIPTION		
+# DATE				2025
+# AUTHOR			ss401533@gmail.com                                           
+#----------------------------------------------------------------------------
+# template found at ~/.vim/sh_header.temp
+
+set -o errexit
+echo "$0" >> ~/db.git/command_history.txt | ts >> ~/db.git/command_history_timestamped.txt
+
+cat <<EOF | batcat --style=plain --paging=never --language sh --theme TwoDark
+rustc helloworld.rs
+EOF
+
