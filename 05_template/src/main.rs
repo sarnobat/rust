7047011d6fad573add8e57f5cef5c0e25d0780fd
Sridhar Sarnobat <ss401533@gmail.com>
2025-07-21 19:12:25 -0400

    2025-07-21: main.rs

--- 05_template/src/main.rs
+++ 05_template/src/main.rs
@@ -25,8 +25,12 @@ fn main() {
                 if line.trim() == "exit" {
                     break;
                 }
+                /// 1) Print to stdout
                 println!("[debug] {}", line);
 
+                /// 3) Parse File path
+
+                /// 2) Regex capture groups extracted and read separately
                 let re = Regex::new(r"^(?P<dir>.*/)?(?P<file>[^/]+?)\.(?P<ext>[^./]+)$").unwrap();
                 let path = line;
                 if let Some(caps) = re.captures(&path) {
@@ -37,9 +41,27 @@ fn main() {
                     println!("Directory: {}", dir);
                     println!("Filename: {}", file);
                     println!("Extension: {}", ext);
+
+                    /// 5) Read and write to a map
+
+                    // 11) create json object
+
                 } else {
                     println!("No match");
-                }
+                    
+                    /// 6) Call a shell program instead
+                        let output = Command::new("date")
+        .arg("+%s")
+        .output()
+        .expect("Failed to execute command");
+
+    if output.status.success() {
+        let stdout = String::from_utf8_lossy(&output.stdout);
+        println!("Epoch seconds: {}", stdout.trim());
+    } else {
+        eprintln!("Command failed with status: {:?}", output.status);
+    }
+
             }
             Err(err) => {
                 eprintln!("Error reading line: {}", err);
