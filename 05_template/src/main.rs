611b243d9fb492ae74ac7016bfc847eba7bfcaa8
Sridhar Sarnobat <ss401533@gmail.com>
2025-07-21 17:58:45 -0400

    2025-07-21: main.rs

--- 05_template/src/main.rs
+++ 05_template/src/main.rs
@@ -1,43 +1,17 @@
 use std::io::{self, BufRead};
 
-
-// use std::io::stdin;
-use clap::{Command, Arg};
-// use std::process;
+use clap::{Arg, Command};
 
 fn main() {
-  //   let matches = Command::new("My App")
-//         .version("1.0")
-//         .author("Your Name")
-//         .about("Greets you by name")
-//         .arg(
-//             Arg::with_name("name")
-//                 .short("n")
-//                 .long("name")
-//                 .value_name("NAME")
-//                 .help("Your name")
-//                 .takes_value(true),
-//         )
-//         .arg(
-//             Arg::with_name("help")
-//                 .long("help")
-//                 .help("Prints help information")
-//         )
-//         .get_matches();
     let matches = Command::new("example")
-        .arg(
-            Arg::new("debug")
-                .long("debug")
-                .help("Enable debug mode")
-        )
+        .arg(Arg::new("debug").long("debug").help("Enable debug mode"))
         .get_matches();
-
 
     if matches.contains_id("debug") {
         println!("Debug mode is on");
     }
     println!("Hello ");
-   let stdin = io::stdin();
+    let stdin = io::stdin();
     let handle = stdin.lock();
 
     for line_result in handle.lines() {
@@ -54,18 +28,4 @@ fn main() {
             }
         }
     }
-    // 
-//     if let Some(input) = matches.get_one::<String>("input") {
-//         println!("Using input: {}", input);
-//     }
-//     if matches.is_present("help") {
-//         App::new("My App").print_help().unwrap();
-//         process::exit(0);
-//     }
-// 
-//     if let Some(name) = matches.value_of("name") {
-//         println!("Hello {}", name);
-//     } else{
-//         println!("Hello ");
-//     }
 }
