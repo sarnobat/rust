8418d87d7038451a9a0006907d2e01ce26a9ad23
Sridhar Sarnobat <ss401533@gmail.com>
2025-08-17 01:29:25 -0400

    2025-08-17: Cargo.lock Cargo.toml main.rs

--- 08_day_number/src/main.rs
+++ 08_day_number/src/main.rs
@@ -1,3 +1,12 @@
+use chrono::{Datelike, Local};
+
 fn main() {
-    println!("Hello, world!");
+
+    // Current date
+    let today = Local::now().date_naive();
+
+    // Day of year (1..=366)
+    let day_of_year: u32 = today.ordinal();
+
+    println!("{}", day_of_year);
 }
