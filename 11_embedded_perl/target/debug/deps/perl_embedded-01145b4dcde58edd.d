2845f5b3ae8882f84328e5a32b1e6eec4f9cdcf5
Sridhar Sarnobat <ss401533@gmail.com>
2025-10-08 21:10:43 -0400

    2025-10-08: obj

--- 11_embedded_perl/target/debug/deps/perl_embedded-01145b4dcde58edd.d
+++ 11_embedded_perl/target/debug/deps/perl_embedded-01145b4dcde58edd.d
@@ -1,5 +1,6 @@
-/Volumes/git/github/depot_parent/rust/11_embedded_perl/target/debug/deps/perl_embedded-01145b4dcde58edd: src/main.rs
+/Volumes/git/github/depot_parent/rust/11_embedded_perl/target/debug/deps/perl_embedded-01145b4dcde58edd: src/main.rs src/../embedded_lib/Graph-Easy-0.64/lib/Graph/Easy.pm
 
-/Volumes/git/github/depot_parent/rust/11_embedded_perl/target/debug/deps/perl_embedded-01145b4dcde58edd.d: src/main.rs
+/Volumes/git/github/depot_parent/rust/11_embedded_perl/target/debug/deps/perl_embedded-01145b4dcde58edd.d: src/main.rs src/../embedded_lib/Graph-Easy-0.64/lib/Graph/Easy.pm
 
 src/main.rs:
+src/../embedded_lib/Graph-Easy-0.64/lib/Graph/Easy.pm:
