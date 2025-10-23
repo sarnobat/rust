2da8c89074864e422ae65f3695df5e6c1938ebbc
Sridhar Sarnobat <ss401533@gmail.com>
2025-10-23 15:33:00 -0400

    2025-10-22: main.rs

--- 16_opengl/src/main.rs
+++ 16_opengl/src/main.rs
@@ -1,202 +1,51 @@
-use glutin::event::{Event, WindowEvent, VirtualKeyCode, ElementState};
-use glutin::event_loop::ControlFlow;
+use glutin::{
+    event::{Event, WindowEvent},
+    event_loop::ControlFlow,
+};
+use glow::HasContext;
 
-fn compile_shader(
-    gl: &glow::Context,
-    ty: u32,
-    src: &str,
-) -> Result<glow::NativeShader, String> {
-    unsafe {
-        let shader = gl.create_shader(ty).map_err(|e| format!("{:?}", e))?;
-        gl.shader_source(shader, src);
-        gl.compile_shader(shader);
-        if !gl.get_shader_compile_status(shader) {
-            let log = gl.get_shader_info_log(shader);
-            gl.delete_shader(shader);
-            Err(log)
-        } else {
-            Ok(shader)
-        }
-    }
-}
-
-fn link_program(
-    gl: &glow::Context,
-    vert: glow::NativeShader,
-    frag: glow::NativeShader,
-) -> Result<glow::NativeProgram, String> {
-    unsafe {
-        let program = gl.create_program().map_err(|e| format!("{:?}", e))?;
-        gl.attach_shader(program, vert);
-        gl.attach_shader(program, frag);
-        gl.link_program(program);
-        if !gl.get_program_link_status(program) {
-            let log = gl.get_program_info_log(program);
-            gl.delete_program(program);
-            Err(log)
-        } else {
-            // shaders can be detached/deleted after linking
-            gl.detach_shader(program, vert);
-            gl.detach_shader(program, frag);
-            gl.delete_shader(vert);
-            gl.delete_shader(frag);
-            Ok(program)
-        }
-    }
-}
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Create event loop and windowed context
-    let event_loop = glutin::event_loop::EventLoop::new();
+fn main() {
+    // Create event loop and window
+    let el = glutin::event_loop::EventLoop::new();
     let wb = glutin::window::WindowBuilder::new()
-        .with_title("Rust OpenGL Hello World")
+        .with_title("Hello OpenGL (Rust)")
         .with_inner_size(glutin::dpi::LogicalSize::new(800.0, 600.0));
 
-    let windowed_context = glutin::ContextBuilder::new()
+    // Create OpenGL context
+    let ctx = glutin::ContextBuilder::new()
         .with_vsync(true)
-        .build_windowed(wb, &event_loop)?;
+        .build_windowed(wb, &el)
+        .unwrap();
 
-    // Make the context current
-    let windowed_context = unsafe { windowed_context.make_current().map_err(|(_, e)| e)? };
+    // Make current
+    let ctx = unsafe { ctx.make_current().unwrap() };
 
-    // Load glow using the context's get_proc_address
-    let gl = unsafe {
-        glow::Context::from_loader_function(|s| {
-            windowed_context.get_proc_address(s) as *const _
-        })
-    };
+    // Load OpenGL via glow
+    let gl = unsafe { glow::Context::from_loader_function(|s| ctx.get_proc_address(s) as *const _) };
 
-    unsafe {
-        // Setup GL state
-        gl.clear_color(0.1, 0.12, 0.15, 1.0);
-    }
-
-    // Simple vertex + fragment shader
-    let vertex_shader_src = r#"
-        #version 330 core
-        layout (location = 0) in vec2 aPos;
-        layout (location = 1) in vec3 aColor;
-        out vec3 vColor;
-        void main() {
-            vColor = aColor;
-            gl_Position = vec4(aPos, 0.0, 1.0);
-        }
-    "#;
-
-    let fragment_shader_src = r#"
-        #version 330 core
-        in vec3 vColor;
-        out vec4 FragColor;
-        void main() {
-            FragColor = vec4(vColor, 1.0);
-        }
-    "#;
-
-    // Compile + link
-    let vert = compile_shader(&gl, glow::VERTEX_SHADER, vertex_shader_src)
-        .map_err(|e| format!("vertex shader compile error: {}", e))?;
-    let frag = compile_shader(&gl, glow::FRAGMENT_SHADER, fragment_shader_src)
-        .map_err(|e| format!("fragment shader compile error: {}", e))?;
-    let program = link_program(&gl, vert, frag)
-        .map_err(|e| format!("program link error: {}", e))?;
-
-    // Vertex data for a triangle: position (x,y) and color (r,g,b)
-    let vertices: [f32; 15] = [
-        // x,    y,     r,   g,   b
-         0.0,  0.6,   1.0, 0.3, 0.2, // top
-        -0.6, -0.6,   0.2, 0.7, 0.3, // left
-         0.6, -0.6,   0.2, 0.4, 1.0, // right
-    ];
-
-    unsafe {
-        // Create VAO and VBO
-        let vao = gl.create_vertex_array().unwrap();
-        let vbo = gl.create_buffer().unwrap();
-
-        gl.bind_vertex_array(Some(vao));
-        gl.bind_buffer(glow::ARRAY_BUFFER, Some(vbo));
-        let vbo_slice_u8 = std::slice::from_raw_parts(
-            vertices.as_ptr() as *const u8,
-            vertices.len() * std::mem::size_of::<f32>(),
-        );
-        gl.buffer_data_u8_slice(glow::ARRAY_BUFFER, vbo_slice_u8, glow::STATIC_DRAW);
-
-        // position attribute (location = 0)
-        let stride = (5 * std::mem::size_of::<f32>()) as i32;
-        gl.enable_vertex_attrib_array(0);
-        gl.vertex_attrib_pointer_f32(0, 2, glow::FLOAT, false, stride, 0);
-
-        // color attribute (location = 1)
-        gl.enable_vertex_attrib_array(1);
-        gl.vertex_attrib_pointer_f32(
-            1,
-            3,
-            glow::FLOAT,
-            false,
-            stride,
-            (2 * std::mem::size_of::<f32>()) as i32,
-        );
-
-        // unbind
-        gl.bind_buffer(glow::ARRAY_BUFFER, None);
-        gl.bind_vertex_array(None);
-    }
-
-    // Run event loop and draw
-    event_loop.run(move |event, _, control_flow| {
+    // Event loop
+    el.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Wait;
 
         match event {
-            Event::RedrawRequested(_) => {
-                unsafe {
-                    gl.clear(glow::COLOR_BUFFER_BIT);
-                    gl.use_program(Some(program));
-
-                    // bind VAO and draw
-                    let vao = gl.get_integer_v(glow::VERTEX_ARRAY_BINDING) as u32;
-                    // We created a VAO earlier; easier to store it, but rebind by recreating variable:
-                    // For clarity, rebind the VAO properly. (We didn't save it outside unsafe earlier.)
-                    // However gl.get_integer_v returned 0 -> better to keep owning VAO variable outside.
-                    // For simplicity, we'll rebind by reading from program's vertex array we created earlier.
-                    // To keep code small, re-create VAO variable earlier would be cleaner. But gl.bind... below:
-                }
-
-                // For correctness rebind VAO using stored name: (recompute by re-creating? simpler: store it)
-                // To avoid extra state complexity, we will instead rebind by re-creating the VAO handle above.
-                // (Because the run closure moved and above handles weren't saved out; the safe approach is to keep
-                // VAO in an Arc or move into closure. For brevity we'll simply draw with vertex arrays bound:
-                unsafe {
-                    // Draw using the buffer we prepared earlier:
-                    // Because we previously bound & populated the VBO and VAO, GL keeps them bound.
-                    // We cannot guarantee bindings survive across the make_current; normally they do.
-                    // Try drawing:
-                    gl.bind_vertex_array(Some(1)); // best-effort; if invalid, nothing drawn, but typical drivers give VAO id >=1
-                    gl.draw_arrays(glow::TRIANGLES, 0, 3);
-                    gl.bind_vertex_array(None);
-                    windowed_context.swap_buffers().unwrap();
-                }
-            }
-
             Event::MainEventsCleared => {
-                // request a redraw
-                windowed_context.window().request_redraw();
+                // Redraw request every frame
+                ctx.window().request_redraw();
             }
 
-            Event::WindowEvent { event, .. } => match event {
-                WindowEvent::CloseRequested => {
-                    *control_flow = ControlFlow::Exit;
-                }
-                WindowEvent::KeyboardInput { input, .. } => {
-                    if let Some(VirtualKeyCode::Escape) = input.virtual_keycode {
-                        if input.state == ElementState::Pressed {
-                            *control_flow = ControlFlow::Exit;
-                        }
-                    }
-                }
-                _ => {}
+            Event::RedrawRequested(_) => unsafe {
+                // Clear the screen to a bluish color
+                gl.clear_color(0.2, 0.4, 0.6, 1.0);
+                gl.clear(glow::COLOR_BUFFER_BIT);
+                ctx.swap_buffers().unwrap();
             },
 
-            _ => {}
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => *control_flow = ControlFlow::Exit,
+
+            _ => (),
         }
     });
 }
