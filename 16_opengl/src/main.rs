2e4a85634b1d2badb5d635a892fdad9fbae1d731
Sridhar Sarnobat <ss401533@gmail.com>
2025-10-23 15:35:17 -0400

    2025-10-22: main.rs

--- 16_opengl/src/main.rs
+++ 16_opengl/src/main.rs
@@ -5,47 +5,73 @@ use glutin::{
 use glow::HasContext;
 
 fn main() {
-    // Create event loop and window
     let el = glutin::event_loop::EventLoop::new();
     let wb = glutin::window::WindowBuilder::new()
-        .with_title("Hello OpenGL (Rust)")
+        .with_title("Rust OpenGL Triangle")
         .with_inner_size(glutin::dpi::LogicalSize::new(800.0, 600.0));
-
-    // Create OpenGL context
     let ctx = glutin::ContextBuilder::new()
         .with_vsync(true)
         .build_windowed(wb, &el)
         .unwrap();
-
-    // Make current
     let ctx = unsafe { ctx.make_current().unwrap() };
-
-    // Load OpenGL via glow
     let gl = unsafe { glow::Context::from_loader_function(|s| ctx.get_proc_address(s) as *const _) };
 
-    // Event loop
-    el.run(move |event, _, control_flow| {
-        *control_flow = ControlFlow::Wait;
+    // --- Shader setup ---
+    let vs_src = r#"#version 330 core
+        const vec2 verts[3] = vec2[3](
+            vec2( 0.0,  0.5),
+            vec2(-0.5, -0.5),
+            vec2( 0.5, -0.5)
+        );
+        void main() {
+            gl_Position = vec4(verts[gl_VertexID], 0.0, 1.0);
+        }"#;
 
-        match event {
-            Event::MainEventsCleared => {
-                // Redraw request every frame
-                ctx.window().request_redraw();
+    let fs_src = r#"#version 330 core
+        out vec4 color;
+        void main() { color = vec4(0.9, 0.3, 0.2, 1.0); }"#;
+
+    unsafe {
+        let program = gl.create_program().unwrap();
+
+        let vs = gl.create_shader(glow::VERTEX_SHADER).unwrap();
+        gl.shader_source(vs, vs_src);
+        gl.compile_shader(vs);
+        assert!(gl.get_shader_compile_status(vs), "vertex shader failed");
+
+        let fs = gl.create_shader(glow::FRAGMENT_SHADER).unwrap();
+        gl.shader_source(fs, fs_src);
+        gl.compile_shader(fs);
+        assert!(gl.get_shader_compile_status(fs), "fragment shader failed");
+
+        gl.attach_shader(program, vs);
+        gl.attach_shader(program, fs);
+        gl.link_program(program);
+        assert!(gl.get_program_link_status(program), "link failed");
+
+        gl.delete_shader(vs);
+        gl.delete_shader(fs);
+
+        // 🔧 FIX: Create and bind a dummy VAO
+        let vao = gl.create_vertex_array().unwrap();
+        gl.bind_vertex_array(Some(vao));
+
+        el.run(move |event, _, control_flow| {
+            *control_flow = ControlFlow::Wait;
+            match event {
+                Event::MainEventsCleared => ctx.window().request_redraw(),
+                Event::RedrawRequested(_) => {
+                    gl.clear_color(0.1, 0.1, 0.12, 1.0);
+                    gl.clear(glow::COLOR_BUFFER_BIT);
+                    gl.use_program(Some(program));
+                    gl.draw_arrays(glow::TRIANGLES, 0, 3);
+                    ctx.swap_buffers().unwrap();
+                }
+                Event::WindowEvent { event: WindowEvent::CloseRequested, .. } => {
+                    *control_flow = ControlFlow::Exit;
+                }
+                _ => (),
             }
-
-            Event::RedrawRequested(_) => unsafe {
-                // Clear the screen to a bluish color
-                gl.clear_color(0.2, 0.4, 0.6, 1.0);
-                gl.clear(glow::COLOR_BUFFER_BIT);
-                ctx.swap_buffers().unwrap();
-            },
-
-            Event::WindowEvent {
-                event: WindowEvent::CloseRequested,
-                ..
-            } => *control_flow = ControlFlow::Exit,
-
-            _ => (),
-        }
-    });
+        });
+    }
 }
