0fa86ce54df877c532dd51829d54033d8f5b1d70
Sridhar Sarnobat <ss401533@gmail.com>
2025-10-31 17:37:54 -0400

    2025-10-31: .rustc_info.json hello_metal Cargo.toml Makefile main.rs main.rs

--- 21_opengl_vulkan/src/main.rs
+++ 21_opengl_vulkan/src/main.rs
@@ -1,3 +1,37 @@
+use vulkano::instance::{Instance, InstanceCreateInfo};
+use vulkano::device::{Device, DeviceCreateInfo, QueueCreateInfo};
+use vulkano::device::physical::{PhysicalDevice, PhysicalDeviceType};
+
 fn main() {
-    println!("Hello, world!");
+    // 1. Create Vulkan instance
+    let instance = Instance::new(InstanceCreateInfo::default())
+        .expect("failed to create instance");
+
+    // 2. Pick a physical device
+    let physical = PhysicalDevice::enumerate(&instance)
+        .filter(|p| p.properties().device_type == PhysicalDeviceType::DiscreteGpu)
+        .next()
+        .unwrap_or_else(|| PhysicalDevice::enumerate(&instance).next().unwrap());
+
+    println!("Using device: {} (type: {:?})", 
+        physical.properties().device_name, 
+        physical.properties().device_type
+    );
+
+    // 3. Create a logical device and a queue
+    let queue_family = physical.queue_families()
+        .find(|&q| q.supports_graphics())
+        .expect("couldn't find a graphics queue family");
+
+    let (device, mut queues) = Device::new(
+        physical,
+        DeviceCreateInfo {
+            queue_create_infos: vec![QueueCreateInfo::family(queue_family)],
+            ..Default::default()
+        }
+    ).expect("failed to create device");
+
+    let _queue = queues.next().unwrap();
+
+    println!("Minimal Vulkan setup done!");
 }
