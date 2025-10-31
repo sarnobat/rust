319985251c1f1100a0bb040d68ca116c1395ece8
Sridhar Sarnobat <ss401533@gmail.com>
2025-10-31 17:38:15 -0400

    2025-10-31: .rustc_info.json metal_matmul metal_matmul.d

--- 13_gpu_metal_cuda_matrix_multiplication/target/debug/metal_matmul.d
+++ 13_gpu_metal_cuda_matrix_multiplication/target/debug/metal_matmul.d
@@ -1,1 +1,1 @@
-/Volumes/git/github/depot_parent/rust/14_gpu_metal_cuda_matrix_multiplication/target/debug/metal_matmul: /Volumes/git/github/depot_parent/rust/14_gpu_metal_cuda_matrix_multiplication/src/main.rs
+/Volumes/git/github/depot_parent/rust/13_gpu_metal_cuda_matrix_multiplication/target/debug/metal_matmul: /Volumes/git/github/depot_parent/rust/13_gpu_metal_cuda_matrix_multiplication/src/main.rs
