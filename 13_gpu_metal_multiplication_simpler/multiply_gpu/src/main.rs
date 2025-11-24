e202004036bb6859c715ae70e488709daf1f86e9
Sridhar Sarnobat <ss401533@gmail.com>
2025-11-23 23:10:02 -0500

    2025-11-23: Cargo.toml main.rs

--- 13_gpu_metal_multiplication_simpler/multiply_gpu/src/main.rs
+++ 13_gpu_metal_multiplication_simpler/multiply_gpu/src/main.rs
@@ -1,44 +1,65 @@
-use std::env;
-use std::thread;
+use metal::*;
+use objc::rc::autoreleasepool;
 
 fn main() {
-    // Parse factor from first argument, default to 3
-    let args: Vec<String> = env::args().collect();
-    let factor: i32 = args.get(1)
-        .and_then(|s| s.parse().ok())
-        .unwrap_or(3);
+    autoreleasepool(|| {
+        let device = Device::system_default().unwrap();
+        let command_queue = device.new_command_queue();
 
-    // Input array
-    let input = vec![0, 1, 1, 0, 1, 0, 1, 0];
-    let mut output = vec![0; input.len()];
+        let input = vec![0f32, 1., 1., 0., 1., 0., 1., 0.];
+        let count = input.len();
 
-    // Simple parallelism: spawn threads
-    let num_threads = 4;
-    let chunk_size = (input.len() + num_threads - 1) / num_threads;
-    let mut handles = Vec::new();
+        let buffer = device.new_buffer(
+            (count * std::mem::size_of::<f32>()) as u64,
+            MTLResourceOptions::CPUCacheModeDefaultCache,
+        );
 
-    for chunk in input.chunks(chunk_size) {
-        // clone the chunk so it is 'static for the thread
-        let chunk_owned = chunk.to_vec();
-        let factor = factor;
-        let handle = thread::spawn(move || {
-            chunk_owned.iter().map(|&x| x * factor).collect::<Vec<i32>>()
-        });
-        handles.push(handle);
-    }
-
-    // Collect results
-    let mut idx = 0;
-    for h in handles {
-        let chunk_res = h.join().unwrap();
-        for v in chunk_res {
-            output[idx] = v;
-            idx += 1;
+        unsafe {
+            std::ptr::copy_nonoverlapping(input.as_ptr(), buffer.contents() as *mut f32, count);
         }
-    }
 
-    // Print output
-    println!("Input: {:?}", input);
-    println!("Factor: {}", factor);
-    println!("Output: {:?}", output);
+        let kernel_src = r#"
+        kernel void multiply(device float* data [[buffer(0)]], uint id [[thread_position_in_grid]]) {
+            if (id < 1000) data[id] *= 3.0;
+        }"#;
+
+        let library = device
+            .new_library_with_source(kernel_src, &CompileOptions::new())
+            .unwrap();
+        let function = library.get_function("multiply", None).unwrap();
+
+        // <-- Updated for metal-rs 0.32+
+        let descriptor = ComputePipelineDescriptor::new();
+        descriptor.set_compute_function(Some(&function));
+        let pipeline = device.new_compute_pipeline_state(&descriptor).unwrap();
+
+        let command_buffer = command_queue.new_command_buffer();
+        let encoder = command_buffer.new_compute_command_encoder();
+        encoder.set_compute_pipeline_state(&pipeline);
+        encoder.set_buffer(0, Some(&buffer), 0);
+
+        let threads = MTLSize {
+            width: count as u64,
+            height: 1,
+            depth: 1,
+        };
+        let threads_per_group = MTLSize {
+            width: 1,
+            height: 1,
+            depth: 1,
+        };
+
+        encoder.dispatch_threads(threads, threads_per_group);
+        encoder.end_encoding();
+        command_buffer.commit();
+        command_buffer.wait_until_completed();
+
+        let mut output = vec![0f32; count];
+        unsafe {
+            std::ptr::copy_nonoverlapping(buffer.contents() as *const f32, output.as_mut_ptr(), count);
+        }
+
+        println!("Input: {:?}", input);
+        println!("Output: {:?}", output);
+    });
 }
